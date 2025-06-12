5b82a5733b11472a502ad9aded17dc4ea3abfcc7
Parveen Saroha <parveensaroha2002@gmail.com>
2025-06-13 00:52:48 +0530

    file added

--- Day4/imageBorder.py
+++ Day4/imageBorder.py
@@ -9,8 +9,8 @@ import numpy as np
 img1 = cv2.imread("D:\\Computer Vision\\2. Computer Vision\\CV\\lion.jpg")
 img1 = cv2.resize(img1,(1000,600))
 #creating image border 
-brdr = cv2.copyMakeBorder(img1,10,10,5,5,cv2.BORDER_CONSTANT,value = [255,0,125])
+border = cv2.copyMakeBorder(img1,10,10,5,5,cv2.BORDER_CONSTANT,value = [255,0,125])
 
-cv2.imshow("res",brdr)
+cv2.imshow("image with border",border)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
